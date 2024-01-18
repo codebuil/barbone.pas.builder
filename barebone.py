0f089d3f8f0807fc53ed245da9e73d2d66922722
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-18 19:42:34 +0000

    Add files via upload

--- barebone.py
+++ barebone.py
@@ -30,43 +30,44 @@ class BareboneBuilder:
         self.copy_button = tk.Button(self.root, text="new file", command=self.copy_file)
         self.copy_button.pack(pady=5)
 
-    def execute_command(self, command):
+    def execute_command(self, command,shows:bool):
         try:
             
             result = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True, text=True)
             self.text_area.insert(tk.END, result)
         except subprocess.CalledProcessError as e:
-            self.text_area.insert(tk.END,f"Error executing command:\n{e.output}")
+            if shows:
+                self.text_area.insert(tk.END,f"Error executing command:\n{e.output}")
 
     def build_kernel(self):
         filename = tk.filedialog.askopenfilename(title="Select file")
         self.text_area.delete(1.0, tk.END)
-        self.execute_command("rm -f kernel.bin")
-        self.execute_command("rm -f /tmp/kernel.o")
-        self.execute_command("rm -f /tmp/ppas.sh")
-        self.execute_command("mv -f ppas.sh /tmp/")
-        self.execute_command("mv -f link.res /tmp")
-        self.execute_command("nasm -felf32 -o /tmp/boot.o ./file/boot.s")
+        self.execute_command("mv -f kernel.bin /tmp/null",False)
+        self.execute_command("mv -f /tmp/kernel.o /tmp/null",False)
+        self.execute_command("mv -f /tmp/ppas.sh /tmp/null",False)
+        self.execute_command("mv -f ppas.sh /tmp/null",False)
+        self.execute_command("mv -f link.res /tmp/null",False)
+        self.execute_command("nasm -felf32 -o /tmp/boot.o ./file/boot.s",True)
         fff=f'fpc -Cn -CcCDECL -O2 -Xs -Xs -Xt -Pi386 -Tlinux "$1"'.replace("$1",filename)
         
-        self.execute_command(fff)
-        self.execute_command("mv -f ppas.sh /tmp")
-        self.execute_command("mv link.res /tmp")
-        self.execute_command("mv -f ./kernel.o /tmp")
-        self.execute_command("gcc ./file/link.ld /tmp/boot.o /tmp/kernel.o -o /tmp/kernel.bin -nostdlib")
-        self.execute_command("grub-file --is-x86-multiboot /tmp/kernel.bin")
-        self.execute_command("mv /tmp/kernel.bin ./")
+        self.execute_command(fff,True)
+        self.execute_command("mv -f ppas.sh /tmp",False)
+        self.execute_command("mv -f link.res /tmp",False)
+        self.execute_command("mv -f ./kernel.o /tmp",False)
+        self.execute_command("gcc ./file/link.ld /tmp/boot.o /tmp/kernel.o -o /tmp/kernel.bin -nostdlib",True)
+        self.execute_command("grub-file --is-x86-multiboot /tmp/kernel.bin",True)
+        self.execute_command("mv -f /tmp/kernel.bin ./",True)
 
     def run_kernel(self):
         self.text_area.delete(1.0, tk.END)
-        self.execute_command("qemu-system-i386 -kernel kernel.bin")
+        self.execute_command("qemu-system-i386 -kernel kernel.bin",True)
 
     def copy_file(self):
         self.text_area.delete(1.0, tk.END)
         filename = tk.filedialog.asksaveasfilename(title="Select file")
         if filename:
             shutil.copy( f"./file/new",filename+".pas")
-            self.text_area.insert(tk.END, f"File {filename} copied \n")
+            self.text_area.insert(tk.END, f"File {filename} copied \n",True)
 
 
 if __name__ == "__main__":
